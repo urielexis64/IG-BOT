9205b861cea6a4b4aec087cc4c3f9157810a7080
Uriel Aispuro <urielalexis64@gmail.com>
2020-10-09 04:02:15 -0600

    Informative comments added

--- login.py
+++ login.py
@@ -10,14 +10,22 @@ class Login:
         self.driver = driver
         self.username = username
         self.password = password
+    
     def signIn(self):
+        #Open Instagram website
         self.driver.get('https://www.instagram.com/')
+
+        #locate Username input text
         uid = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#loginForm > div > div:nth-child(1) > div > label > input')))
         uid.click()
         uid.send_keys(self.username)
-        pass = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#loginForm > div > div:nth-child(2) > div > label > input')))
-        pass.click()
-        pass.send_keys(self.password)
+
+        #locate Password input text
+        password = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#loginForm > div > div:nth-child(2) > div > label > input')))
+        password.click()
+        password.send_keys(self.password)
+
+        #locate and click Login button
         button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#loginForm > div > div:nth-child(3) > button')))
         button.click()
         time.sleep(5)
